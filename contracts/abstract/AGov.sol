eb8ef9fbbbcbd5fff494e42da6db6835093d6a58
seunghwalee <seunghwalee@wemix.com>
2023-06-22 13:34:51 +0900

    AGov,GovImp : changed variables position for mainnet

--- contracts/abstract/AGov.sol
+++ contracts/abstract/AGov.sol
@@ -21,6 +21,13 @@ abstract contract AGov is GovChecker, IGov {
     mapping(uint256 => address) internal stakers;
     mapping(address => uint256) public stakerIdx;
 
+    //For a node duplicate check
+    // mainnet value is here
+    // mapping(bytes32=>bool) internal checkNodeInfo;
+    mapping(bytes=>bool) internal checkNodeName;
+    mapping(bytes=>bool) internal checkNodeEnode;
+    mapping(bytes32=>bool) internal checkNodeIpPort;
+
     // For enode
     struct Node {
         bytes name;
