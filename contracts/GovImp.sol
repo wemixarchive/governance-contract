16849896b3bec89342823fb1aa39845d78996d12
dongjunna <nadongjun@coinplug.com>
2023-10-10 07:54:07 +0000

    Add GIU-03 INTEGER UNDERFLOW WILL CAUSE reInitV3() TO REVERT

--- contracts/GovImp.sol
+++ contracts/GovImp.sol
@@ -1241,22 +1241,6 @@ contract GovImp is
         }
     }
 
-    function reInitV3(uint256[] memory indices, address[] memory newRewards) external reinitializer(3) onlyOwner{
-        unchecked {
-            for(uint256 i=0; i<indices.length;i++){
-                address oldReward = rewards[i];
-                address newReward = newRewards[i-1];
-                rewards[i] = newReward;
-                rewardIdx[newReward] = i;
-                rewardIdx[oldReward] = 0;
-            }
-        }
-        //for the testnet
-        checkNodeName[nodes[40].name] = true;
-        checkNodeEnode[nodes[40].enode] = true;
-        checkNodeIpPort[keccak256(abi.encodePacked(nodes[40].ip, nodes[40].port))] = true;
-    }
-
     function setMembersForMigration(
         uint256 id,
         address staker,
