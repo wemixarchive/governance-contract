57b2f17d6c664f719b134d07885da758600db72d
eomti-wm <eomti@wemade.com>
2024-08-14 14:16:42 +0900

    fix: (GGI-02) Remove unused value

--- contracts/gateway/GovGatewayImp.sol
+++ contracts/gateway/GovGatewayImp.sol
@@ -197,7 +197,6 @@ contract GovGatewayImp is GovChecker, UUPSUpgradeable, ReentrancyGuardUpgradeabl
         }
 
         uint256 voteLength = govGateway.voteLength();
-        uint256 ballotLength = govGateway.ballotLength();
 
         voteList = new Vote[](totalVoters);
         uint256 voteListIdx = 0;
@@ -235,7 +234,6 @@ contract GovGatewayImp is GovChecker, UUPSUpgradeable, ReentrancyGuardUpgradeabl
         if (totalVoters == 0) return voteList;
 
         uint256 voteLength = govGateway.voteLength();
-        uint256 ballotLength = govGateway.ballotLength();
         voteList = new uint256[](totalVoters);
         uint256 voteListIdx = 0;
 
@@ -253,7 +251,6 @@ contract GovGatewayImp is GovChecker, UUPSUpgradeable, ReentrancyGuardUpgradeabl
 
     function getTargetTotalVoters(uint256 ballotId) public view returns (uint256, uint256, uint256) {
         address[] memory ballotAddressList = getBallotStorageAddressList();
-        IGovGateway govGateway = IGovGateway(getGovAddress());
         IBallotStorage curBallotStorage;
 
         uint256 totalVoterIdx = 0;
@@ -320,12 +317,8 @@ contract GovGatewayImp is GovChecker, UUPSUpgradeable, ReentrancyGuardUpgradeabl
 
     function getVoteList(uint256 ballotId) external view returns (Vote[] memory voteList) {
         address[] memory ballotAddressList = getBallotStorageAddressList();
-        IGovGateway govGateway = IGovGateway(getGovAddress());
-        IBallotStorage curBallotStorage;
-        uint256 ballotLength = govGateway.ballotLength();
         (uint256 targetVoter, uint256 totalVoterIdx, uint256 addressIdx) = getTargetTotalVoters(ballotId);
         if (ballotAddressList.length > addressIdx) {
-            IGovGateway ballotGateway = IGovGateway(ballotAddressList[addressIdx]);
             voteList = new Vote[](targetVoter);
             uint256 j = 0;
             for (uint256 i = (totalVoterIdx - targetVoter + 1); i <= totalVoterIdx; i++) {
